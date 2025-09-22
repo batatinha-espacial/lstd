1bb51047b81746f986cc2ad96eb667812d501032
Antônio Breno <coisasaletaorias@gmail.com>
2025-09-22 20:01:49 -0300

    added table.push

--- src/table.rs
+++ src/table.rs
@@ -13,8 +13,18 @@ fn len(_: &Lua, t: LuaTable) -> LuaResult<LuaInteger> {
     Ok(i as LuaInteger)
 }
 
+fn push(lua: &Lua, (t, args): (LuaTable, LuaVariadic<LuaValue>)) -> LuaResult<LuaInteger> {
+    let mut len = len(lua, t.clone())?;
+    for i in args {
+        len += 1;
+        t.set(len, i)?;
+    }
+    Ok(len)
+}
+
 pub fn module(lua: &Lua) -> LuaResult<LuaTable> {
     let exports = lua.create_table()?;
     exports.set("len", lua.create_function(len)?)?;
+    exports.set("push", lua.create_function(push)?)?;
     Ok(exports)
 }
